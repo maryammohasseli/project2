b64a4d0ee6e78de6aaf33e59466a2d8324160c9b
maryammohasseli <maryam.mohasseli@gmail.com>
2017-03-14 19:18:57 +1100

    title renamed

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Portfolio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="css/bootstrap.css"/>
     <link rel="stylesheet" href="css/style.css"/>
